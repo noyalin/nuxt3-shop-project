b4e20f51aaf0250d924a1b06b6346a628004747c
noyalin <1375162594@qq.com>
2023-04-22 01:09:18 +0800

    modify~

--- src/pages/list.vue
+++ src/pages/list.vue
@@ -1,18 +1,62 @@
 <script setup lang="ts">
-const foo = useFoo()
-console.log(foo)
+interface Query {
+  value?: ''
+}
+const query: Query = reactive({})
 
-const { x, y } = useMouse()
+const dataSource = [
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+]
+
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+]
 </script>
 
 <template>
   <div>
-    list
-    <div>x: {{ x }}</div>
-    <div>y: {{ y }}</div>
-    <div>foo:{{ foo }}</div>
-    <TestComp></TestComp>
+    <a-row class="search-box">
+      <a-col :span="8">
+        <a-input v-model:value="query.value" placeholder="name" />
+      </a-col>
+      <a-col :span="8" c><a-button type="primary">搜索</a-button></a-col>
+    </a-row>
+
+    <a-row>
+      <a-col :span="24">
+        <a-table :data-source="dataSource" :columns="columns"></a-table>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.search-box {
+  margin-bottom: 24px;
+}
+</style>
